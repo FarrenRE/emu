af96af526a3c0bed2d345f23f82160971ea184e7
aadlhq <farren.esman@aadl.org.au>
2019-08-21 15:53:58 +1000

    separate file for themes variable

---
+++ src/components/themes.js
@@ -0,0 +1,86 @@
+const themes = {
+  adma: {
+    org: {
+      name: 'Association for Data-driven Marketing and Advertising',
+      abbr: 'ADMA',
+      abn: '34 002 909 800',
+      addr: 'Level 6 50 Carrington Street, Sydney NSW 2000',
+      website: 'https://www.adma.com.au',
+      social: {
+        fb: 'http://www.facebook.com/adma.com.au',
+        tw: 'http://www.twitter.com/adma',
+        li: 'https://www.linkedin.com/company/adma',
+      },
+      logos: {
+        header: 'https://adma-public-assets.s3-ap-southeast-2.amazonaws.com/edm/EMU/ADMA_assoc_header-logos_400x75.jpg',
+        lockup: 'https://adma-public-assets.s3-ap-southeast-2.amazonaws.com/edm/EMU/ADMA_assoc_aadl-lockup_110x40.png',
+      },
+    },
+    colors: {
+      primary: '#CC0000',
+      primaryLighter: '#e60000',
+      primaryDarker: '#b30000',
+      secondary: '#FFFFFF',
+      bgColor: '#F6F6F6',
+      link: '#CC0000',
+      footer: '#363636'
+    }
+  },
+  iq: {
+    org: {
+      name: 'Association for Data-driven Marketing and Advertising',
+      abbr: 'ADMA IQ',
+      abn: '34 002 909 800',
+      addr: 'Level 6 50 Carrington Street, Sydney NSW 2000',
+      website: 'https://www.adma.com.au/iq',
+      social: {
+        fb: 'http://www.facebook.com/adma.com.au',
+        tw: 'http://www.twitter.com/adma',
+        li: 'https://www.linkedin.com/company/adma',
+      },
+      logos: {
+        header: 'https://adma-public-assets.s3-ap-southeast-2.amazonaws.com/edm/EMU/IQ_assoc_header-logos_400x75.jpg',
+        lockup: 'https://adma-public-assets.s3-ap-southeast-2.amazonaws.com/edm/EMU/ADMA_assoc_aadl-lockup_110x40.png',
+      },
+    },
+    colors: {
+      primary: '#CC0000',
+      primaryLighter: '#e60000',
+      primaryDarker: '#b30000',
+      secondary: '#FFFFFF',
+      bgColor: '#F6F6F6',
+      link: '#CC0000',
+      footer: '#363636'
+    }
+  },
+  dtc: {
+    org: {
+      name: 'Digital + Technology Collective',
+      abbr: 'D+TC',
+      abn: '34 002 909 800',
+      addr: 'Level 6 50 Carrington Street, Sydney NSW 2000',
+      website: 'https://www.dtcollective.org.au',
+      social: {
+        fb: 'https://www.facebook.com/DigitalTechnologyCollective',
+        tw: 'https://twitter.com/DandTCollective',
+        li: 'https://www.linkedin.com/company/72345',
+      },
+      logos: {
+        header: 'https://adma-public-assets.s3-ap-southeast-2.amazonaws.com/edm/EMU/DTC_assoc_header-logos_400x75.jpg',
+        lockup: 'https://adma-public-assets.s3-ap-southeast-2.amazonaws.com/edm/EMU/DTC_assoc_aadl-lockup_110x40.png',
+      },
+    },
+    colors: {
+      primary: '#553278',
+      primaryLighter: '#663c90',
+      primaryDarker: '#40265a',
+      secondary: '#FFFFFF',
+      bgColor: '#F6F6F6',
+      link: '#804bb4',
+      footer: '#553278',
+      footerStripe: '#363636',
+    }
+  }
+}
+
+export default themes;
