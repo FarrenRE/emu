0d7d3e44fdd8edd77ea125de9c6493d38d3eafb3
FarrenRE <farren.esman@gmail.com>
2019-10-04 12:37:35 +1000

    update schema to isolate campaign variables

--- src/components/themes.js
+++ src/components/themes.js
@@ -6,38 +6,60 @@ const themes = {
       abn: '53 156 305 487',
       addr: 'Level 27, Tower One, 100 Barangaroo Avenue, Sydney, NSW, 2000',
       website: 'https://www.adma.com.au',
-      social: {
-        fb: 'http://www.facebook.com/adma.com.au',
-        tw: 'http://www.twitter.com/adma',
-        li: 'https://www.linkedin.com/company/adma',
-      },
+      social: { fb: 'http://www.facebook.com/adma.com.au', tw: 'http://www.twitter.com/adma', li: 'https://www.linkedin.com/company/adma' },
       logos: {
         header: 'https://adma-public-assets.s3-ap-southeast-2.amazonaws.com/edm/EMU/ADMA_assoc_header-logos_400x75.jpg',
         lockup: 'https://adma-public-assets.s3-ap-southeast-2.amazonaws.com/edm/EMU/lockup/ADMA-acs-lockup_edm-footer.png',
       },
+      colors: {
+        primary: '#CC0000',
+        primaryLighter: '#e60000',
+        primaryDarker: '#b30000',
+        secondary: '#FFFFFF',
+        bgColor: '#F6F6F6',
+        link: '#CC0000',
+        footer: '#363636'
+      }
     },
-    colors: {
-      primary: '#CC0000',
-      primaryLighter: '#e60000',
-      primaryDarker: '#b30000',
-      secondary: '#FFFFFF',
-      bgColor: '#F6F6F6',
-      link: '#CC0000',
-      footer: '#363636'
-    },
-    campaign: {
+    campaigns: {
       monthly: {
         name: 'Monthly',
-        headerType: 'monthly'
+        colors: {
+          primary: '#CC0000',
+          primaryLighter: '#e60000',
+          primaryDarker: '#b30000',
+          secondary: '#FFFFFF',
+          bgColor: '#F6F6F6',
+          link: '#CC0000',
+          footer: '#363636'
+        },
+        header: { type: 'monthly', image: 'https://adma-public-assets.s3-ap-southeast-2.amazonaws.com/edm/EMU/ADMA_assoc_header-logos_400x75.jpg' }
       },
       globalForum: {
         name: 'Global Forum',
-        headerType: 'image',
-        headerImage: ''
+        colors: {
+          primary: '#CC0000',
+          primaryLighter: '#e60000',
+          primaryDarker: '#b30000',
+          secondary: '#FFFFFF',
+          bgColor: '#F6F6F6',
+          link: '#CC0000',
+          footer: '#363636'
+        },
+        header: { type: 'image', image: 'https://via.placeholder.com/600x90.jpg' }
       },
-      test: {
-        name: 'Test Campaign',
-        headerType: 'monthly'
+      ace: {
+        name: 'AC&E Awards',
+        colors: {
+          primary: '#FFC627',
+          primaryLighter: '#ffcf4d',
+          primaryDarker: '#ffbb00',
+          secondary: '#FFFFFF',
+          bgColor: '#F6F6F6',
+          link: '#ffbb00',
+          footer: '#363636'
+        },
+        header: { type: 'image', image: 'https://via.placeholder.com/600x90.jpg' }
       }
     }
   },
@@ -48,29 +70,34 @@ const themes = {
       abn: '53 156 305 487',
       addr: 'Level 27, Tower One, 100 Barangaroo Avenue, Sydney, NSW, 2000',
       website: 'https://www.adma.com.au/iq',
-      social: {
-        fb: 'http://www.facebook.com/adma.com.au',
-        tw: 'http://www.twitter.com/adma',
-        li: 'https://www.linkedin.com/company/adma',
-      },
+      social: { fb: 'http://www.facebook.com/adma.com.au', tw: 'http://www.twitter.com/adma', li: 'https://www.linkedin.com/company/adma' },
       logos: {
         header: 'https://adma-public-assets.s3-ap-southeast-2.amazonaws.com/edm/EMU/IQ_assoc_header-logos_400x75.jpg',
         lockup: 'https://adma-public-assets.s3-ap-southeast-2.amazonaws.com/edm/EMU/lockup/ADMA-acs-lockup_edm-footer.png',
       },
+      colors: {
+        primary: '#CC0000',
+        primaryLighter: '#e60000',
+        primaryDarker: '#b30000',
+        secondary: '#FFFFFF',
+        bgColor: '#F6F6F6',
+        link: '#CC0000',
+        footer: '#363636'
+      },
     },
-    colors: {
-      primary: '#CC0000',
-      primaryLighter: '#e60000',
-      primaryDarker: '#b30000',
-      secondary: '#FFFFFF',
-      bgColor: '#F6F6F6',
-      link: '#CC0000',
-      footer: '#363636'
-    },
-    campaign: {
+    campaigns: {
       monthly: {
         name: 'Monthly',
-        headerType: 'monthly'
+        colors: {
+          primary: '#CC0000',
+          primaryLighter: '#e60000',
+          primaryDarker: '#b30000',
+          secondary: '#FFFFFF',
+          bgColor: '#F6F6F6',
+          link: '#CC0000',
+          footer: '#363636'
+        },
+        header: { type: 'monthly', image: 'https://adma-public-assets.s3-ap-southeast-2.amazonaws.com/edm/EMU/ADMA_assoc_header-logos_400x75.jpg' }
       }
     }
   },
@@ -81,35 +108,48 @@ const themes = {
       abn: '53 156 305 487',
       addr: 'Level 27, Tower One, 100 Barangaroo Avenue, Sydney, NSW, 2000',
       website: 'https://www.iapa.org.au',
-      social: {
-        fb: 'http://www.facebook.com/adma.com.au',
-        tw: 'https://twitter.com/IAPA_org_au',
-        li: 'https://www.linkedin.com/company/institute-of-analytics-professionals',
-      },
+      social: { fb: 'http://www.facebook.com/adma.com.au', tw: 'https://twitter.com/IAPA_org_au', li: 'https://www.linkedin.com/company/institute-of-analytics-professionals' },
       logos: {
         header: 'https://adma-public-assets.s3-ap-southeast-2.amazonaws.com/edm/EMU/IAPA_assoc_header-logos_400x75.jpg',
         lockup: 'https://adma-public-assets.s3-ap-southeast-2.amazonaws.com/edm/EMU/lockup/IAPA-acs-lockup_edm-footer.png',
       },
+      colors: {
+        primary: '#fedb38',
+        primaryLighter: '#fee267',
+        primaryDarker: '#fed41b',
+        secondary: '#333333',
+        bgColor: '#F6F6F6',
+        link: '#fed41b',
+        footer: '#363636'
+      },
     },
-    colors: {
-      primary: '#fedb38',
-      primaryLighter: '#fee267',
-      primaryDarker: '#fed41b',
-      secondary: '#333333',
-      bgColor: '#F6F6F6',
-      link: '#fed41b',
-      footer: '#363636'
-    },
-    campaign: {
+
+    campaigns: {
       monthly: {
         name: 'Monthly',
-        headerType: 'monthly',
-        headerImage: 'https://adma-public-assets.s3-ap-southeast-2.amazonaws.com/edm/EMU/IAPA_assoc_header-logos_400x75.jpg'
+        header: { type: 'monthly', image: 'https://adma-public-assets.s3-ap-southeast-2.amazonaws.com/edm/EMU/IAPA_assoc_header-logos_400x75.jpg' },
+        colors: {
+          primary: '#fedb38',
+          primaryLighter: '#fee267',
+          primaryDarker: '#fed41b',
+          secondary: '#333333',
+          bgColor: '#F6F6F6',
+          link: '#fed41b',
+          footer: '#363636'
+        },
       },
-      globalForum: {
-        name: 'Global Forum',
-        headerType: 'image',
-        headerImage: ''
+      advancingAnalytics: {
+        name: 'Advancing Analytics',
+        header: { type: 'image', image: 'https://adma-public-assets.s3-ap-southeast-2.amazonaws.com/edm/EMU/iapa/aa19/AA_2019_EDM_Banner_600x63.jpg' },
+        colors: {
+          primary: '#01A19A',
+          primaryLighter: '#01b2a9',
+          primaryDarker: '#017f79',
+          secondary: '#ffffff',
+          bgColor: '#F6F6F6',
+          link: '#37033e',
+          footer: '#363636'
+        },
       },
     }
   },
@@ -120,31 +160,36 @@ const themes = {
       abn: '53 156 305 487',
       addr: 'Level 27, Tower One, 100 Barangaroo Avenue, Sydney, NSW, 2000',
       website: 'https://www.dtcollective.org.au',
-      social: {
-        fb: 'https://www.facebook.com/DigitalTechnologyCollective',
-        tw: 'https://twitter.com/DandTCollective',
-        li: 'https://www.linkedin.com/company/72345',
-      },
+      social: { fb: 'https://www.facebook.com/DigitalTechnologyCollective', tw: 'https://twitter.com/DandTCollective', li: 'https://www.linkedin.com/company/72345' },
       logos: {
         header: 'https://adma-public-assets.s3-ap-southeast-2.amazonaws.com/edm/EMU/DTC_assoc_header-logos_400x75.jpg',
         lockup: 'https://adma-public-assets.s3-ap-southeast-2.amazonaws.com/edm/EMU/lockup/DTC-acs-lockup_edm-footer.png',
       },
+      colors: {
+        primary: '#553278',
+        primaryLighter: '#663c90',
+        primaryDarker: '#40265a',
+        secondary: '#FFFFFF',
+        bgColor: '#F6F6F6',
+        link: '#804bb4',
+        footer: '#553278',
+        footerStripe: '#363636',
+      }
     },
-    colors: {
-      primary: '#553278',
-      primaryLighter: '#663c90',
-      primaryDarker: '#40265a',
-      secondary: '#FFFFFF',
-      bgColor: '#F6F6F6',
-      link: '#804bb4',
-      footer: '#553278',
-      footerStripe: '#363636',
-    },
-    campaign: {
+    campaigns: {
       monthly: {
         name: 'Monthly',
-        headerType: 'monthly',
-        headerImage: ''
+        header: { type: 'monthly', image: '' },
+        colors: {
+          primary: '#553278',
+          primaryLighter: '#663c90',
+          primaryDarker: '#40265a',
+          secondary: '#FFFFFF',
+          bgColor: '#F6F6F6',
+          link: '#804bb4',
+          footer: '#553278',
+          footerStripe: '#363636',
+        }
       }
     }
   }
