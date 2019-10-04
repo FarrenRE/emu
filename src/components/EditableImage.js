557a23217cc2e92a307fa876a5fb58a7ef400758
FarrenRE <farren.esman@gmail.com>
2019-10-04 16:36:00 +1000

    add popupbox for image attribute editing

--- src/components/EditableImage.js
+++ src/components/EditableImage.js
@@ -1,28 +1,63 @@
 import React from 'react';
 import _ from 'lodash';
+import { PopupboxManager, PopupboxContainer } from 'react-popupbox';
+import "react-popupbox/dist/react-popupbox.css";
 
 class EditableImage extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      src: ''
+    };
     this.id = this.setId();
-    this.myRef = React.createRef();
+    this.imgRef = React.createRef();
   }
   setId = () => {
     return _.uniqueId('editableimg_');
   }
+  openPopupbox = () => {
+    const content = (
+      <div>
+        <span>Image src: </span>
+        <input onChange={this.onInputUpdate} name='src' type='text' />
+        <button onClick={this.updateData}>Submit</button>
+      </div>
+    )
+    PopupboxManager.open({ content })
+  }
+  updateData = () => {
+    console.log('updated');
+    PopupboxManager.close();
+  }
+  /** Generic handler for <input> updates. Updates state corresponding to name value */
+  onInputUpdate = (evt) => {
+    this.setState({ [evt.target.name]: evt.target.value });
+  }
   render() {
+    // pop-up box config: https://fraina.github.io/react-popupbox/
+    const popupboxConfig = {
+      className: 'editableimg-popup',
+      titleBar: {
+        enable: true,
+        text: 'Image editor'
+      },
+      fadeIn: false,
+    }
+
     return (
-      <img
-        ref={this.myRef}
-        className={`editableimg ${this.props.activeID === this.id ? 'active' : ''}`}
-        id={this.id}
-        onClick={(e) => { this.props.setActiveEdit(e, this) }}
-        src={this.props.src}
-        alt={this.props.alt} border={this.props.border}
-        style={this.props.style}
-        width={this.props.width}
-        height={this.props.height} />
+      <div>
+        <img
+          ref={this.imgRef}
+          className={`editableimg ${this.props.activeID === this.id ? 'active' : ''}`}
+          id={this.id}
+          onClick={(e) => { this.openPopupbox() }}
+          src={this.props.src}
+          alt={this.props.alt} border={this.props.border}
+          style={this.props.style}
+          width={this.props.width}
+          height={this.props.height} />
+        <PopupboxContainer {...popupboxConfig} />
+      </div>
     );
   }
 }
