1157170e6e30f265bc86c99732b16c59424699f5
FarrenRE <farren.esman@gmail.com>
2019-10-11 15:01:06 +1100

    working demo

--- src/components/test.js
+++ src/components/test.js
@@ -1,33 +1,39 @@
 import React from 'react';
-
+// https://medium.com/@jordanmmartin/quickstart-guide-to-using-cloudinary-upload-widget-v2-in-react-4c4d9041d55e
 class Test extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       greeting: 'hello',
-      noun: 'world'
+      noun: 'world',
+      url: 'some sample url'
+    }
+  }
+  showWidget = () => {
+    this.widget.open();
+  }
+  checkUploadResult = (resultEvent) => {
+    if (resultEvent.event === 'success') {
+      console.log(`url: ${resultEvent.info.url}`);
+      this.setState({ url: resultEvent.info.url });
+    } else {
+      console.log('upload error');
     }
   }
   render() {
-    return ( 
+    this.widget = window.cloudinary.createUploadWidget({
+      cloudName: 'adma',
+      uploadPreset: 'my_preset'
+    },
+      (error, result) => { this.checkUploadResult(result) });
+
+    return (
       <div>
-        <h1>{ this.state.greeting } { this.state.noun }</h1>
-        <form>
-        <input
-            type='text'
-            name='greeting'
-            value={ this.state.greeting }
-            onChange={ this.myChangeHandler } />
-          <input
-            type='text'
-            name='noun'
-            value={ this.state.noun }
-            onChange={ this.myChangeHandler } />
-        </form>
+        <button onClick={this.showWidget}>{this.state.url}</button>
       </div>
     );
   }
-  myChangeHandler = ( evt ) => {
+  myChangeHandler = (evt) => {
     let name = evt.target.name;
     let value = evt.target.value;
     this.setState({ [name]: value });
